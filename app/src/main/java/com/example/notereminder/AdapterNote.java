8a919c42ec6c18bd8799b0f1110ba17a80970d4b
konstrictor <jonkur96@gmail.com>
2020-11-18 00:40:49 +0200

    coded Recycler AdapterNote

---
+++ app/src/main/java/com/example/notereminder/AdapterNote.java
@@ -0,0 +1,54 @@
+package com.example.notereminder;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdapterNote extends RecyclerView.Adapter<AdapterNote.ViewHolder> {
+
+    ArrayList<Note> notes;
+
+    public AdapterNote(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_note, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        final Note note = notes.get(position);
+        holder.lbl_description.setText(note.description);
+        holder.lbl_echeance.setText(note.echeance);
+        holder.lbl_priorite.setText(note.strPriorite());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView lbl_description, lbl_echeance, lbl_priorite;
+        View view;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            view = itemView;
+            lbl_description = itemView.findViewById(R.id.lbl_description);
+            lbl_echeance = itemView.findViewById(R.id.lbl_echeance);
+            lbl_priorite = itemView.findViewById(R.id.lbl_priorite);
+        }
+    }
+}
--- app/src/main/java/com/example/notereminder/Note.java
+++ app/src/main/java/com/example/notereminder/Note.java
@@ -1,13 +1,36 @@
 package com.example.notereminder;
 
 public class Note {
-    public String id, description, couleur, echeance, priorite;
+    public String id, description, couleur, echeance;
+    Integer priorite;
 
-    public Note(String id, String description, String couleur, String echeance, String priorite) {
+    public Note(String id, String description, String couleur, String echeance, Integer priorite) {
         this.id = id;
         this.description = description;
         this.couleur = couleur;
         this.echeance = echeance;
         this.priorite = priorite;
+    }
+
+    public String strPriorite(){
+        String str = "";
+        switch (priorite){
+            case 0:
+                str = "trop bas";
+                break;
+            case 1:
+                str = "bas";
+                break;
+            case 2:
+                str = "moyenne";
+                break;
+            case 3:
+                str = "elevée";
+                break;
+            case 4:
+                str = "trop élevee";
+                break;
+        }
+        return "priorité " + str;
     }
 }
